42ef27beef9b57572a42fccea2627e0b9cff226b
Dim <om1234777@gmail.com>
2020-08-12 22:20:25 +0300

    add task 1

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const questionOne = prompt('Один из последних просмотренных фильмов?', '');
+const questionTwo = prompt('На сколько оцените его?', '');
+const questionThree = prompt('Один из последних просмотренных фильмов?', '');
+const questionFour = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[questionOne] = questionTwo;
+personalMovieDB.movies[questionThree] = questionFour;
+
+console.log(personalMovieDB);
